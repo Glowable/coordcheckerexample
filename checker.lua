f172aad708a0ef830af841a381ed6a4bd4405036
Glow <leovonandion@gmail.com>
2023-04-18 19:34:49 +0200

    first commit

---
+++ checker.lua
@@ -0,0 +1,27 @@
+Citizen.CreateThread(function()
+    local ped = PlayerPedId()
+    local inArea = false
+    local toggledArea = false
+    local coords
+    local dist
+    while true do
+        Wait(0)
+        coords = GetEntityCoords(ped)
+        dist = #(coords - vector3(478.11, 4813.41, -60.38)) --Coordinates to check
+        if dist < 4 then --Radius of the area
+           if not toggledArea then
+              toggledArea = true
+              inArea = true
+              print("Player is in the area!") -- Triggered when the player is in the area
+              
+           end
+        else
+           if toggledArea then
+              inArea = false
+              toggledArea = false
+              print("Played is no longer in the area!")
+
+          end
+        end
+    end
+end)
