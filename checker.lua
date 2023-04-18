590020f5b7b4da8d735b7f4853312670b9d56f5f
Glowable <leovonandion@gmail.com>
2023-04-18 19:49:39 +0200

    Update checker.lua

--- checker.lua
+++ checker.lua
@@ -5,7 +5,7 @@ Citizen.CreateThread(function()
     local coords
     local dist
     while true do
-        Wait(0)
+        Wait(10)
         coords = GetEntityCoords(ped)
         dist = #(coords - vector3(-1256.48, -3025.69, -49.5)) --Coordinates to check
         if dist < 4 then --Radius of the area
